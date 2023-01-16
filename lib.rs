0f2795dddbec5b157419d34645979285a917d71a
Robert Kunze <robert@quantgene.com>
2023-01-16 13:08:52 +0100

    :star2: adds ability to pause and control volume

--- lib.rs
+++ lib.rs
@@ -8,17 +8,45 @@
 //! // example.ts
 //! await play("examples/music.mp3");
 //! ```
-
-use deno_bindgen::deno_bindgen;
+use rodio::Decoder;
+use rodio::Sink;
 use std::io::BufReader;
 
-#[deno_bindgen(non_blocking)]
-pub fn play(filename: &str) {
-    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-    let sink = rodio::Sink::try_new(&handle).unwrap();
+static mut GLOBAL_DATA: Vec<Sink> = Vec::new();
 
-    let file = std::fs::File::open(&filename).unwrap();
-    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-    // `play` runs from a different thread.
-    sink.sleep_until_end();
+unsafe fn ptr_to_string(ptr: *const u8, len: usize) -> String {
+    std::str::from_utf8_unchecked(std::slice::from_raw_parts(ptr, len)).to_owned()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn play(buf: *const u8, len: usize, volume: f32) {
+    //!# Safety
+    let filename = ptr_to_string(buf, len);
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&handle).unwrap();
+
+    let file = std::fs::File::open(filename).unwrap();
+    sink.append(Decoder::new(BufReader::new(file)).unwrap());
+    sink.set_volume(volume);
+
+    let idx: usize;
+    {
+        GLOBAL_DATA.push(sink);
+        idx = GLOBAL_DATA.len() - 1;
+    } // write lock should be dropped here...
+
+    GLOBAL_DATA[idx].sleep_until_end();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn set_volume(index: i32, new_volume: f32) {
+    //!# Safety
+    GLOBAL_DATA[index as usize].set_volume(new_volume);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn pause(index: i32) {
+    //!# Safety
+    GLOBAL_DATA[index as usize].pause();
+    GLOBAL_DATA[index as usize].stop();
 }
--- mod.ts
+++ mod.ts
@@ -1,1 +1,44 @@
-export { play } from "./bindings/bindings.ts";
+const OS_PREFIX = Deno.build.os === "windows" ? "" : "lib";
+const OS_SUFFIX = Deno.build.os === "windows"
+  ? ".dll"
+  : Deno.build.os === "darwin"
+  ? ".dylib"
+  : ".so";
+
+function getLibraryPath(lib: string): string {
+  lib = `${OS_PREFIX}${lib}${OS_SUFFIX}`;
+  const libPath = "./lib";
+  if (libPath) {
+    return `${libPath}/${lib}`;
+  } else {
+    return lib;
+  }
+}
+
+let audioCounter = 0;
+const deno_audio = Deno.dlopen(getLibraryPath("deno_audio"), {
+  play: {
+    parameters: ["pointer", "u32", "f32"],
+    result: "void",
+    nonblocking: true,
+  },
+  pause: { parameters: ["i32"], result: "void", nonblocking: false },
+  set_volume: {
+    parameters: ["i32", "f32"],
+    result: "void",
+    nonblocking: false,
+  },
+});
+export function play(a0: string, volume: number) {
+  const a0_buf = new TextEncoder().encode(a0);
+  // this is very instable at the moment... needs to be replaced with a better library
+  const a0_ptr = Deno.UnsafePointer.of(a0_buf);
+  deno_audio.symbols.play(a0_ptr, a0_buf.length, volume);
+  return audioCounter++;
+}
+export function pause(idx: number) {
+  deno_audio.symbols.pause(idx);
+}
+export function setVolume(idx: number, new_volume: number) {
+  deno_audio.symbols.set_volume(idx, new_volume);
+}
